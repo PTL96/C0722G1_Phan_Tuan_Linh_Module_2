230b81ef8484bc9a75ca18bb6d7b5fce4febaea0
PTL96 <skyfreedom45@gmail.com>
2022-09-07 20:08:36 +0700

    sua bai

--- module_2/src/ss1_java/exercise/Hello.java
+++ module_2/src/ss1_java/exercise/Hello.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Hello {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello");
+        System.out.println("Gửi lời chào đến ?");
+        String name = scanner.nextLine();
+        System.out.println("Hello "+name);
     }
 }
