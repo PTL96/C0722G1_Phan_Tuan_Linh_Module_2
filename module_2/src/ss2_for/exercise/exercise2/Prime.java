5580e4579ac5d26f8178cfafef661e3f22f2787c
PTL96 <skyfreedom45@gmail.com>
2022-09-07 20:26:47 +0700

    nop bai

--- module_2/src/ss2_for/exercise/exercise2/Prime.java
+++ module_2/src/ss2_for/exercise/exercise2/Prime.java
@@ -2,10 +2,21 @@ package ss2_for.exercise.exercise2;
 
 public class Prime {
     public static void main(String[] args) {
-        for (int i = 0; i <20 ; i++) {
-            if(i==2){
-
+        int number = 20;
+        int count = 0;
+        int n = 2;
+        while (count < number) {
+            int value = 0;
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    value += 1;
+                }
             }
+            if (value == 0) {
+                System.out.println(n);
+                count++;
+            }
+            n++;
         }
     }
 }
